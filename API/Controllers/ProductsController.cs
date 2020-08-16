76c6cc53275a3535e5315ed40f2aaee4bde7241c
Anton Ryabuhin <Lordd871@gmail.com>
2020-08-16 19:29:37 +1000

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()        
+            => "this will be a list of products";        
+    }
+}
